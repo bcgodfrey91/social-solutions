cc5df7dfb205f4982c238f80e2e24dee414e716b
Ben Godfrey <bcgodfrey91@gmail.com>
2016-10-31 12:54:11 -0600

    First Commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('hashtag-info', { path: '/' }, function() {
+    this.route('hashtag');
+  });
 });
 
 export default Router;
