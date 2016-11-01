c8e5b5023dacc6b668e2ebbba1e4d63ec2acb09c
Ben Godfrey <bcgodfrey91@gmail.com>
2016-11-01 10:31:00 -0600

    Add hashtag functionality

--- app/routes/hashtag.js
+++ app/routes/hashtag.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return Ember.$.getJSON('/api/tweets-for-hashtag/denver')
+    return Ember.$.getJSON('/api/tweets-for-hashtag/'+params.hashtag)
                   .then(response => response.data);
   }
 });
