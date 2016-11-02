86a869b32378aa3f10881905c97b3c416e27ef1c
Ben Godfrey <bcgodfrey91@gmail.com>
2016-11-02 14:36:40 -0600

    Import ember-lodash

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Controller.extend({
   search: null,
@@ -9,7 +10,7 @@ export default Ember.Controller.extend({
       this.set('search', search)
     },
     searchForParam() {
-      this.transitionToRoute('hashtag', this.search)
+      this.transitionToRoute('hashtag', '#' + this.search)
     }
   }
 });
