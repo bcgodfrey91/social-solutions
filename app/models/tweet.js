f9fe9639bb931386b075746e03ad5413ba5571b5
Ben Godfrey <bcgodfrey91@gmail.com>
2016-11-07 15:50:39 -0700

    Add filterBy for verified.

--- app/models/tweet.js
+++ app/models/tweet.js
@@ -2,5 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   text: DS.attr('string'),
-  user: DS.attr()
-});
+  name: DS.attr('string'),
+  followers: DS.attr('number'),
+  location: DS.attr('string'),
+  verified: DS.attr('boolean', {defaultValue: false}),
+
+  info: Ember.computed.uniq('location'),
+  renderLocations: Ember.computed('info', function(){
+    return `${this.get('info')}`
+  })
+})
