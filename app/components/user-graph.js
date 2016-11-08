3ec2212d2717bd69421bdc3e0e15fd59c89e78e1
Ben Godfrey <bcgodfrey91@gmail.com>
2016-11-08 15:06:47 -0700

    Issues with highcharts.

--- app/components/user-graph.js
+++ app/components/user-graph.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
+import defaultTheme from '../themes/default-theme';
 
 export default Ember.Component.extend({
 
-  
 });
--- app/controllers/hashtag.js
+++ app/controllers/hashtag.js
@@ -1,8 +1,44 @@
 import Ember from 'ember';
+import defaultTheme from '../themes/default-theme';
+
 
 const { computed, get } = Ember;
 
 export default Ember.Controller.extend({
+  //graph
+  chartOptions: {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      allowDecimals: false,
+      title: {
+        text: 'Fruit eaten'
+      }
+    }
+  },
+
+  chartData: [
+    {
+      name: 'Jane',
+      data: [5, 0, 20]
+    },
+    {
+      name: 'John',
+      data: [25, 30, 15]
+    }
+  ],
+
+  theme: defaultTheme
+
+});
+
   // grabbing model info
   tweets: computed.alias('model'),
   userLocations: computed.mapBy('tweets', 'location'),
@@ -12,7 +48,7 @@ export default Ember.Controller.extend({
   getMinFollowers: computed.min('getFollowers'),
   getMaxFollowers: computed.max('getFollowers'),
   sumAllFollowers: computed.sum('getFollowers'),
-  findAverageFollowers: computed('sumAllFollowers', function() {
+  findAverageFollowers: computed('sumAllFollowers', 'tweets', function() {
     let sumAllFollowers = get(this, 'sumAllFollowers')
 
     return (sumAllFollowers / 100).toFixed(0)
